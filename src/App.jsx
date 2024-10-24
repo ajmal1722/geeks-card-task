9a5a5e4d25b77fca807cefa01df424b7d56a261d
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-10-24 20:30:50 +0530

    range input functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     date: '6 Days | 5 Nights',
     image: 'https://plus.unsplash.com/premium_photo-1697730314165-2cd71dc3a6a4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bXVubmFyfGVufDB8fDB8fHww',
     price: '32,579',
-    discountPrice: '25,800',
+    discountPrice: '20,000',
     pickup: 'Cochin Airport / Railway Station',
     
   },{
@@ -16,22 +16,46 @@ function App() {
     date: '4 Days | 3 Nights',
     image: 'https://thumbs.dreamstime.com/z/zanskar-blue-river-where-leh-ladak-india-95570243.jpg',
     price: '32,579',
-    discountPrice: '25,800',
+    discountPrice: '30,000',
     pickup: 'Sringar Airport',
   },{
     name: 'Munnar Thekkady Alleppey and Cherri Tour',
     date: '3 Days | 2 Nights',
     image: 'https://luxeglamp.com/wp-content/uploads/2023/02/luxeglamp-Projects-Munnar.jpg',
     price: '32,577',
-    discountPrice: '25,800',
+    discountPrice: '45,000',
     pickup: 'Cochin Airport',
   },]
 
+  const [rangeValue, setRangeValue] = useState(100);
+  const [movies, setMovies] = useState(data)
+
+  const handleChange = (e) => {
+    setRangeValue(e.target.value);
+    console.log(e.target.value);
+
+    const sortedMovies = data.filter(item => {
+      return parseInt(item.discountPrice) <= rangeValue
+    })
+    // console.log(sortedMovies)
+    setMovies(sortedMovies)
+  }
+
   return (
     <>
-      <div className='flex gap-5 md:px-20 mt-20'>
+      <div className='mt-10 flex justify-end mx-20'>
+        <input 
+          type="range"
+          value={rangeValue}
+          onChange={handleChange}
+        />
+        <button>
+          {rangeValue}
+        </button>
+      </div>
+      <div className='flex gap-5 md:px-20 mt-10'>
         {
-          data.map(card => {
+          movies.map(card => {
             return (
               <Card key={data.name} card={card} />
             )
